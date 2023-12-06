66f6cc6a6c2110096fd64285d7066dc7d4040b35
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-12-06 14:07:20 +0100

    Add staging for combustion

---
+++ dbt/models/stg_mastr__combustion.sql
@@ -0,0 +1,51 @@
+{{
+    config(
+        materialized = 'table',
+        indexes=[
+            {'columns': ['coordinate'], 'type': 'gist'}
+        ]
+    )
+}}
+
+with source as (
+    select * from {{ source('public', 'combustion_extended') }}
+),
+
+
+renamed as (
+
+    select
+        --general
+        "EinheitMastrNummer" as mastr_id,
+        "EinheitBetriebsstatus" as operating_status,
+        "NetzbetreiberpruefungStatus" as grid_operator_inspection,
+        --dates
+        CASE
+        WHEN "Inbetriebnahmedatum" IS NOT NULL OR "GeplantesInbetriebnahmedatum" IS NOT NULL THEN
+            concat(
+            date_part('year', "Inbetriebnahmedatum"),
+            date_part('year', "GeplantesInbetriebnahmedatum")
+        )::integer
+        ELSE
+            NULL
+        END as installation_year,
+        "Inbetriebnahmedatum" as commissioning_date,
+        "GeplantesInbetriebnahmedatum" as planned_commissioning_date,
+        "DatumDownload" as download_date,
+        --technical
+        "Nettonennleistung" as power,
+        "Energietraeger" as energy_carrier,
+        --owner
+        "AnlagenbetreiberMastrNummer" as unit_owner_mastr_id,
+        --location
+        "Gemeindeschluessel" as municipality_id,
+        "Gemeinde" as municipality,
+        left("Gemeindeschluessel", 5) as district_id,
+        "Landkreis" as district,
+        "Postleitzahl" as zip_code,
+        st_setsrid(st_point("Laengengrad", "Breitengrad"), 4326) as coordinate
+    from source
+
+)
+
+select * from renamed
