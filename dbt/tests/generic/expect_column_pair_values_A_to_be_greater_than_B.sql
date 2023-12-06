dc30ea7488e71091237bf9d38ecb9bb4cb479783
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-12-06 12:50:28 +0100

    Add factor and where clause to generic test

--- dbt/tests/generic/expect_column_pair_values_A_to_be_greater_than_B.sql
+++ dbt/tests/generic/expect_column_pair_values_A_to_be_greater_than_B.sql
@@ -1,4 +1,4 @@
-{% test expect_column_pair_values_A_to_be_greater_than_B(model, column_A, column_B, or_equal=False) %}
+{% test expect_column_pair_values_A_to_be_greater_than_B(model, column_A, column_B, where, factor_column_A=1, or_equal=False) %}
 
 SELECT 
     *
@@ -6,9 +6,13 @@ FROM
     {{ model }}
 WHERE 
     {% if or_equal %}
-            {{ column_B }} > {{ column_A }}
+            {{ column_B }} > {{ column_A }} * {{ factor_column_A }}
     {% else %}
-        {{ column_B }} >= {{ column_A }}
+        {{ column_B }} >= {{ column_A }} * {{ factor_column_A }}
+    {% endif %}
+    
+    {% if where %}
+        AND {{ where }}
     {% endif %}
 
 {% endtest %}
