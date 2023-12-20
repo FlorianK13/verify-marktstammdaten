54abe01ca45093a7bfa5efe246841c2bd87c2746
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-12-20 16:19:33 +0100

    Add additional commands

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,10 @@
 
 conda activate verify-marktstammdaten # this environment needs to have the needed packages installed
 
+docker-compose up -d
+
+python load_raw_data.py
+
 cd dbt
 
 dbt run
@@ -11,3 +15,5 @@ dbt test --store-failures
 cd ..
 
 python failed_tests_to_sqlite.py
+
+docker-compose down
