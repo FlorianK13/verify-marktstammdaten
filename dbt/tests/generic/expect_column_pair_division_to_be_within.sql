74b146d9b73dd5642ef39181fa5a192cce43a286
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-03-19 15:07:46 +0100

    Add test for wind power vs rotor diameter

--- dbt/tests/generic/expect_column_pair_division_to_be_within.sql
+++ dbt/tests/generic/expect_column_pair_division_to_be_within.sql
@@ -1,4 +1,4 @@
-{% test expect_column_pair_division_to_be_within(model, column_A, column_B, min_value, max_value) %}
+{% test expect_column_pair_division_to_be_within(model, column_A, column_B, min_value, max_value, condition, square_column_B=False) %}
 
     WITH no_zeros_in_column_b AS (
         SELECT
@@ -8,9 +8,20 @@
         WHERE
             -- Avoid dividing by zero
             {{ column_B }} != 0
+            {% if condition %}
+            AND {{ condition }} 
+            {% endif %}
+
     )
     SELECT * FROM no_zeros_in_column_b
         WHERE
-        ({{ column_A }} / {{ column_B }}) <= {{ min_value }}
-        OR ({{ column_A }} / {{ column_B }}) >= {{ max_value }}
+        {% if square_column_B %}
+            ({{ column_A }} / {{ column_B }}) / {{ column_B }}<= {{ min_value }}
+            OR ({{ column_A }} / {{ column_B }} /  {{ column_B }} ) >= {{ max_value }}
+        {% else %}
+            ({{ column_A }} / {{ column_B }}) <= {{ min_value }}
+            OR ({{ column_A }} / {{ column_B }}) >= {{ max_value }}
+        {% endif %}
+
+        
 {% endtest %}
