551487ddb3eb024a0c124a36adc23b6b8dc05c69
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-07-24 16:37:14 +0200

    Merge solar and wind plot to one

--- paper/figure_distance_to_district.py
+++ paper/figure_distance_to_district.py
@@ -37,39 +37,54 @@ def get_data_wind():
     gdf_districts = gpd.GeoSeries.from_wkb(df_wind["area_geometry_with_buffer"], crs="EPSG:4326").to_crs("EPSG:4839")
     return gdf, gdf_districts
 
-df_wind, districts_wind = get_data_wind()
-df_solar, districts_solar = get_data_solar()
+def generate_data():
+    df_wind, districts_wind = get_data_wind()
+    df_solar, districts_solar = get_data_solar()
 
-wind_distance = df_wind["geometry"].distance(districts_wind, align=False).to_numpy()
-solar_distance = df_solar["geometry"].distance(districts_solar, align=False).to_numpy()
-print(f"Lowest distance of wind turbines to district is {np.min(wind_distance)}m")
-print(f"Lowest distance of solar systems to district is {np.min(solar_distance)}m")
-
-wind_capped_distance_in_km = np.fmin(wind_distance, np.array([60000]*len(wind_distance)))/1000
-SOLAR_CAPPED_DISTANCE = 300
-solar_capped_distance_in_km = np.fmin(solar_distance, np.array([SOLAR_CAPPED_DISTANCE*1000]*len(solar_distance)))/1000
+    wind_distance = df_wind["geometry"].distance(districts_wind, align=False).to_numpy()
+    solar_distance = df_solar["geometry"].distance(districts_solar, align=False).to_numpy()
+    print(f"Lowest distance of wind turbines to district is {np.min(wind_distance)}m")
+    print(f"Lowest distance of solar systems to district is {np.min(solar_distance)}m")
 
 
-fig, (ax, ax2) = plt.subplots(1,2,figsize=(6,3))
+    wind_capped_distance_in_km = np.fmin(wind_distance, np.array([SOLAR_CAPPED_DISTANCE*1000]*len(wind_distance)))/1000
+    solar_capped_distance_in_km = np.fmin(solar_distance, np.array([SOLAR_CAPPED_DISTANCE*1000]*len(solar_distance)))/1000
+
+    np.save('solar_capped_distance_in_km.npy', solar_capped_distance_in_km)
+    np.save('wind_capped_distance_in_km.npy', wind_capped_distance_in_km)
+
+
+    return wind_capped_distance_in_km, solar_capped_distance_in_km
+
+GENERATE_DATA = False
+
+if GENERATE_DATA:
+    wind_capped_distance_in_km, solar_capped_distance_in_km = generate_data() 
+else:
+    wind_capped_distance_in_km= np.load('wind_capped_distance_in_km.npy')
+    solar_capped_distance_in_km=np.load('solar_capped_distance_in_km.npy')
+
+
+fig, ax = plt.subplots(1,1,figsize=(6,3))
 FONTSIZE = 13
 
 
 colors = ["#003366", "#2575c4", "#c44e52", "#ff981a"]
 cmap = LinearSegmentedColormap.from_list("my_colormap", colors)
 
-
-ax.hist(wind_capped_distance_in_km, bins=60, range=(0,60), color=colors[0])  
+SOLAR_CAPPED_DISTANCE = 300
+ax.hist(wind_capped_distance_in_km, bins=SOLAR_CAPPED_DISTANCE, range=(0,SOLAR_CAPPED_DISTANCE), color=colors[0], label="wind", alpha=.7)  
+ax.hist(solar_capped_distance_in_km, bins=SOLAR_CAPPED_DISTANCE, range=(0,SOLAR_CAPPED_DISTANCE), color=colors[2], label="solar", alpha=.7)  
 ax.set_xlabel('Distance [km]', fontsize=FONTSIZE)
-ax.set_ylabel('Number Wind Turbines', fontsize=FONTSIZE)
+ax.set_ylabel('Number Incorrect Units', fontsize=FONTSIZE)
 ax.tick_params(axis='both', labelsize=FONTSIZE)
-ax.set_xticks([0, 20, 40, 60])
+ax.legend()
 
 
-ax2.hist(solar_capped_distance_in_km, bins=SOLAR_CAPPED_DISTANCE, range=(0,SOLAR_CAPPED_DISTANCE), color=colors[0])  
-ax2.set_xlabel('Distance [km]', fontsize=FONTSIZE)
-ax2.set_ylabel('Number PV Systems', fontsize=FONTSIZE)
-ax2.tick_params(axis='both', labelsize=FONTSIZE)
-ax2.set_xticks([0, 100, 200, 300])
+
+
+
+ax.set_xticks([0, 100, 200, 300])
 
 plt.tight_layout()
 plt.savefig("figure_appendix_distance.pdf", bbox_inches="tight")
